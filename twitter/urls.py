473a2ca273b7f1396b5385d4ecd51db7b864aebf
ziwen0310! <ziwen.wang@wustl.edu>
2021-05-21 04:23:43 +0800

    authentication complete

--- twitter/urls.py
+++ twitter/urls.py
@@ -19,6 +19,7 @@ from rest_framework import routers
 from accounts.api import views
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
+router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
