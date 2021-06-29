b6256f86b5b47b17da069712bfefba7f0d56788a
ziwen0310! <ziwen.wang@wustl.edu>
2021-06-30 02:19:51 +0800

    friendship-api

--- twitter/urls.py
+++ twitter/urls.py
@@ -19,10 +19,12 @@ from rest_framework import routers
 from accounts.api import views
 from django.conf import settings
 from tweets.api.views import TweetViewSet
+from friendships.api.views import FriendshipViewSet
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
 router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
+router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
