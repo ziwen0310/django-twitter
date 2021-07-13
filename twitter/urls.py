f34346d32c9fd201cd1af6b11d8674e67bbc2229
ziwen0310! <ziwen.wang@wustl.edu>
2021-07-13 20:45:50 +0800

    likes create

--- twitter/urls.py
+++ twitter/urls.py
@@ -22,6 +22,7 @@ from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from comments.api.views import CommentViewSet
+from likes.api.views import LikeViewSet
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
 router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
@@ -29,6 +30,7 @@ router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
+router.register(r'api/likes', LikeViewSet, basename='likes')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
