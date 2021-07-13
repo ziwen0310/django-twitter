d8c473ed31e3ee0931a4a86f09d310acdd5515b3
ziwen0310! <ziwen.wang@wustl.edu>
2021-07-13 20:30:08 +0800

    likes model

--- comments/models.py
+++ comments/models.py
@@ -25,3 +25,10 @@ class Comment(models.Model):
             self.content,
             self.tweet_id,
         )
+
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Comment),
+            object_id=self.id,
+        ).order_by('-created_at')
